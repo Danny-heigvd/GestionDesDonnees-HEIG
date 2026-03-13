376273190fca037ea0c95d58ba36acf184740075
Rui Monnard <rui.monnard@heig-vd.ch>
2026-03-13 15:53:31 +0100

    modification sql

--- initdb/01-schema.sql
+++ initdb/01-schema.sql
@@ -4,7 +4,18 @@ CREATE TABLE fournisseurs (
     contact VARCHAR(150),
     telephone VARCHAR(30),
     email VARCHAR(150),
-    remarques TEXT
+    remarques VARCHAR(150)
+);
+
+CREATE TABLE mobilier (
+    id SERIAL PRIMARY KEY,
+    date_installation DATE,
+    remarque TEXT,
+   id_fournisseur INTEGER NOT NULL REFERENCES fournisseurs(id),
+    id_type_mobilier INTEGER NOT NULL REFERENCES type_mobilier(id),
+    id_lieu INTEGER NOT NULL REFERENCES lieu(id),
+    id_etat INTEGER NOT NULL REFERENCES etat(id),
+    id_materiau INTEGER NOT NULL REFERENCES materiau(id)
 );
  
 CREATE TABLE type_mobilier (
@@ -28,7 +39,22 @@ CREATE TABLE lieu (
     latitude DECIMAL(9,6),
     longitude DECIMAL(9,6)
 );
- 
+
+CREATE TABLE signalement (
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    objet VARCHAR(200),
+    description TEXT,
+    id_type_signalement INTEGER NOT NULL REFERENCES type_signalement(id),
+    id_source_signalement INTEGER NOT NULL REFERENCES source_signalement(id)
+);
+
+CREATE TABLE mobilier_signalement (
+    id SERIAL PRIMARY KEY,
+      id_mobilier INTEGER NOT NULL REFERENCES mobilier(id),
+    id_signalement INTEGER NOT NULL REFERENCES signalement(id),
+);
+
 CREATE TABLE source_signalement (
     id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL
@@ -38,51 +64,7 @@ CREATE TABLE type_signalement (
     id SERIAL PRIMARY KEY,
     libelle VARCHAR(100) NOT NULL
 );
- 
-CREATE TABLE type_intervention (
-    id SERIAL PRIMARY KEY,
-    libelle VARCHAR(100) NOT NULL
-);
- 
-CREATE TABLE technicien_contrat (
-    id SERIAL PRIMARY KEY,
-    date_debut DATE NOT NULL,
-    date_fin DATE
-);
- 
-CREATE TABLE mobilier (
-    id SERIAL PRIMARY KEY,
-    date_installation DATE,
-    remarque TEXT,
-   id_fournisseur INTEGER NOT NULL REFERENCES fournisseurs(id),
-    id_type_mobilier INTEGER NOT NULL REFERENCES type_mobilier(id),
-    id_lieu INTEGER NOT NULL REFERENCES lieu(id),
-    id_etat INTEGER NOT NULL REFERENCES etat(id),
-    id_materiau INTEGER NOT NULL REFERENCES materiau(id)
-);
- 
-CREATE TABLE signalement (
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    objet VARCHAR(200),
-    description TEXT,
-    id_type_signalement INTEGER NOT NULL REFERENCES type_signalement(id),
-    id_source_signalement INTEGER NOT NULL REFERENCES source_signalement(id)
-);
- 
-CREATE TABLE mobilier_signalement (
-    id SERIAL PRIMARY KEY,
-      id_mobilier INTEGER NOT NULL REFERENCES mobilier(id),
-    id_signalement INTEGER NOT NULL REFERENCES signalement(id),
-);
- 
-CREATE TABLE technicien (
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(150) NOT NULL,
-    technicien_contrat VARCHAR(100),
-    id_technicien_contrat INTEGER NOT NULL REFERENCES technicien_contrat(id)
-);
- 
+
 CREATE TABLE intervention (
     id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
@@ -94,3 +76,21 @@ CREATE TABLE intervention (
     id_technicien INTEGER NOT NULL REFERENCES technicien(id),
     id_mobilier INTEGER NOT NULL REFERENCES mobilier(id)
 );
+ 
+CREATE TABLE type_intervention (
+    id SERIAL PRIMARY KEY,
+    libelle VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE technicien (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(150) NOT NULL,
+    id_technicien_contrat INTEGER NOT NULL REFERENCES technicien_contrat(id)
+);
+ 
+CREATE TABLE technicien_contrat (
+    id SERIAL PRIMARY KEY,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL
+);
+ 
